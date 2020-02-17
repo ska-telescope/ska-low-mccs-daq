fa0a8177a0251925200f9fac920370bde4a45c33
Alessio Magro <lessju@gmail.com>
2020-02-17 10:34:46 +0100

    Updated setup files

--- python/setup_python2.py
+++ python/setup_python2.py
@@ -11,5 +11,5 @@ setup(
     description='AAVS Software',
     install_requires=['h5py', 'pyyaml', 'lockfile', 'scapy', 'numpy==1.16.6', 'gitpython',
                       'future', 'astropy==2.0.16', 'matplotlib==2.2.5', 'scipy==1.2.3',
-                      'mongoengine', 'pytz', 'pymongo']
+                      'mongoengine', 'pytz', 'pymongo', 'configparser']
 )
