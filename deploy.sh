a33c78da0cfda972a2de39751bb3c1c62acde9f8
Alessio Magro <lessju@gmail.com>
2020-04-06 16:54:25 +0200

    Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,118 +1,187 @@
-#!/bin/bash
-
-echo "This script has not been updated yet. Do not use."
-exit
+#!/usr/bin/env bash
 
 echo -e "\n==== Configuring AAVS System  ====\n"
 
 # Currently, AAVS LMC has to be installed in this directory
 # DO NOT CHANGE!
-export AAVS_INSTALL_DIRECTORY=/opt/aavs
+export AAVS_INSTALL=/opt/aavs
+
+# Helper function to install required package
+function install_package(){
+    PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $1 | grep "install ok installed")
+    if [[ "" == "$PKG_OK" ]]; then
+      echo "Installing $1."
+      sudo apt-get -qq --yes install $1 > /dev/null
+      return  0 # Return success status
+    else
+      echo "$1 already installed"
+      return 1  # Return fail status (already installed)
+    fi
+}
 
 # Create installation directory tree
 function create_install() {
 
   # Create install directory if it does not exist
-  if [ -z "$AAVS_INSTALL" ]; then
-    export AAVS_INSTALL=$AAVS_INSTALL_DIRECTORY
-
-    # Check whether directory already exists
-    if [ ! -d "$AAVS_INSTALL_DIRECTORY" ]; then
-      # Check whether we have write persmission
-      parent_dir="$(dirname "$AAVS_INSTALL_DIRECTORY")"
-      if [ -w "$parent_dir" ]; then
-        mkdir -p $AAVS_INSTALL_DIRECTORY
-      else
-        sudo mkdir -p $AAVS_INSTALL_DIRECTORY
-        sudo chown $USER $AAVS_INSTALL_DIRECTORY
-      fi
-    fi
+  if [ ! -d "$AAVS_INSTALL" ]; then
+	  sudo mkdir -p $AAVS_INSTALL
+	  sudo chown $USER $AAVS_INSTALL
   fi
 
+  # Create lib directory
   if [ ! -d "$AAVS_INSTALL/lib" ]; then
     mkdir -p $AAVS_INSTALL/lib
   fi
 
+  # Add directory to LD_LIBRARY_PATH
   if [[ ! ":$LD_LIBRARY_PATH:" == *"aavs"* ]]; then
     export LD_LIBRARY_PATH=$AAVS_INSTALL/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH} 
   fi
   
+  # Create bin directory and add to path
   if [ ! -d "$AAVS_INSTALL/bin" ]; then
     mkdir -p $AAVS_INSTALL/bin
+    export PATH=$AAVS_INSTALL/bin${PATH:+:$PATH}
   fi
 
+  # Export AAVS bin directory
   if [ -z "$AAVS_BIN" ]; then
     export AAVS_BIN=$AAVS_INSTALL/bin
+  fi
+
+  # Create include directory
+  if [[ ! -d "$AAVS_INSTALL/include" ]]; then
+    mkdir -p $AAVS_INSTALL/include
+  fi
+  
+  # Create log directory
+  if [[ ! -d "$AAVS_INSTALL/log" ]]; then
+    mkdir -p $AAVS_INSTALL/log
+	chmod a+rw $AAVS_INSTALL/log
+  fi
+
+  # Create python3 virtual environment
+  if [[ ! -d "$AAVS_INSTALL/python3" ]]; then
+    mkdir -p $AAVS_INSTALL/python3
+
+    # Create python virtual environment
+    virtualenv -p python3 $AAVS_INSTALL/python3
+
+    # Add AAVS virtual environment alias to .bashrc
+    if [[ ! -n "`cat ~/.bashrc | grep aavs_python3`" ]]; then
+      echo "alias aavs_python3=\"source \$AAVS_INSTALL/python3/bin/activate\"" >> ~/.bashrc
+      echo "aavs_python3" >> ~/.bashrc
+      echo "Setting virtual environment alias"
+    fi
+
   fi
 }
 
 # Installing required system packages
-echo "Installing required system packages"
-sudo apt-get -q install --force-yes --yes $(grep -vE "^\s*#" requirements.apt  | tr "\n" " ")
+install_package cmake
+install_package git
+install_package git-lfs
+install_package python2.7
+install_package libyaml-dev 
+install_package python3-dev
+install_package python3-virtualenv
+install_package libnuma-dev
+install_package build-essential
+
+# Set up NTP synchronisation
+if install_package ntp; then
+    sudo service ntp reload
+fi
 
 # Create installation directory
 create_install
 echo "Created installed directory tree"
 
-# Installing requirements for eBPF
-#CODENAME=`lsb_release -c`
-#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D4284CDD
-#echo "deb https://repo.iovisor.org/apt "${CODENAME//Codename:}" main" | sudo tee /etc/apt/sources.list.d/iovisor.list
-#sudo apt-get update
-#sudo apt-get -q install --force-yes --yes binutils bcc bcc-tools libbcc-examples python-bcc
+# If software directory is not defined in environment, set it
+if [ -z "$AAVS_SOFTWARE_DIRECTORY" ]; then
+  export AAVS_SOFTWARE_DIRECTORY=`pwd`
+fi
 
-# Build C++ library
-echo "Building Source"
+# Start python virtual environment
+source $AAVS_INSTALL/python3/bin/activate
+
+# Update pip
+pip install -U pip
+
+# Give python interpreter required capabilities for accessing raw sockets and kernel space
+sudo setcap cap_net_raw,cap_ipc_lock,cap_sys_nice,cap_sys_admin,cap_kill+ep $AAVS_INSTALL/python3/bin/python3
+
+# Create a temporary setup directory and cd into it
+if [[ ! -d "third_party" ]]; then
+  mkdir third_party
+fi
+
+pushd third_party || exit
+
+  # Install DAQ
+  if [[ ! -d "aavs-daq" ]]; then
+    git clone https://lessju@bitbucket.org/aavslmc/aavs-daq.git
+
+    pushd aavs-daq/src || exit
+      if [[ ! -d build ]]; then
+        mkdir build
+      fi
+      
+	  # Install DAQ C++ core
+	  pushd build || exit
+        cmake -DCMAKE_INSTALL_PREFIX=$AAVS_INSTALL -DWITH_BCC=OFF ..
+        make -B -j8 install
+      popd
+    popd
+  fi
+
+  # Install PyFabil
+  if [[ ! -d "pyfabil" ]]; then
+    git clone https://lessju@bitbucket.org/lessju/pyfabil.git
+  fi
+  
+  pushd pyfabil || exit
+    python setup.py install
+  popd
+popd
+
+# Install C++ src
 pushd src || exit
-if [ ! -d build ]; then
-  mkdir build
-fi
-pushd build || exit
+  if [ ! -d build ]; then
+    mkdir build
+  fi
 
-# Check if AAVS_PATH exists, and if so cd to it
-if [ -z $AAVS_INSTALL ]; then
-    echo "AAVS_INSTALL not set in termninal"
-    exit 1
-else
-  # Build library
-  cmake -DCMAKE_INSTALL_PREFIX=$AAVS_INSTALL/lib -DWITH_CORRELATOR=OFF ..
-  make -B -j4 install
-fi
-popd || exit
-popd || exit
+  pushd build || exit
+    cmake -DCMAKE_INSTALL_PREFIX=$AAVS_INSTALL/lib -DWITH_CORRELATOR=ON ..
+    make -B -j4 install
+  popd
+popd
+
 
 # Install required python packages
 pushd python || exit
-
-# Check if we are using python in virtual env, if not we need to install
-# numpy via synaptic (not sure why)
-if [ `python -c "import sys; print hasattr(sys, 'real_prefix')"` = "False" ]; then  
-    sudo apt-get -q install --force-yes --yes python-numpy
-fi
-
-pip install -r requirements.pip
-
-# Give python interpreter required capabilities for accessing raw sockets and kernel space
-PYTHON=`which python`
-sudo setcap cap_net_raw,cap_ipc_lock,cap_sys_nice,cap_sys_admin,cap_kill+ep `readlink -f $PYTHON`
-
-# Install DAQ python library
-python setup.py install
+  python setup.py install
+popd
 
 # Link required scripts to bin directory
-pushd pydaq || exit
 FILE=$AAVS_BIN/daq_plotter.py
-if [ ! -e $FILE ]; then
-  ln -s $PWD/daq_plotter.py $FILE
+if [ -e $FILE ]; then
+  sudo rm $FILE
 fi
+sudo ln -s $PWD/python/pydaq/daq_plotter.py $FILE
 
 FILE=$AAVS_BIN/daq_receiver.py
-if [ ! -e $FILE ]; then
-  ln -s $PWD/daq_receiver.py $FILE
+if [ -e $FILE ]; then
+  sudo rm $FILE
 fi
+ln -s $PWD/python/pydaq/daq_receiver.py $FILE
 
-popd || exit
+FILE=$AAVS_BIN/station.py
+if [ -e $FILE ]; then
+  sudo rm $FILE
+fi
+ln -s $PWD/python/pyaavs/station.py $FILE
 
-# Finished with python
-popd || exit
-
+echo ""
+echo "Installation finished. Please check your .bashrc file and source it to update your environment"
+echo ""
