aad93b444c39c3491e736f44b2e9ef7e71a66031
Adam Clemens <ajc@observatorysciences.co.uk>
2022-12-13 16:59:25 +0000

    [MCCS-1279] Pin Pyfabil and Daq versions - Add env vars to Dockerfile specifying commit hashes to use - Modify deploy script to use relevant hashes

--- deploy.sh
+++ deploy.sh
@@ -227,11 +227,15 @@ fi
 pushd third_party || exit
 
   # Install PyFABIL
-  pip install git+https://lessju@bitbucket.org/lessju/pyfabil.git@$PYFABIL_BRANCH --force-reinstall
+  pip install git+https://lessju@bitbucket.org/lessju/pyfabil.git@$PYFABIL_SHA --force-reinstall
 
   # Install DAQ
   if [[ ! -d "aavs-daq" ]]; then
     git clone https://lessju@bitbucket.org/aavslmc/aavs-daq.git
+
+    pushd aavs-daq || exit
+      git reset --hard $AAVS_DAQ_SHA
+    popd
 
     pushd aavs-daq/src || exit
       if [[ ! -d build ]]; then
