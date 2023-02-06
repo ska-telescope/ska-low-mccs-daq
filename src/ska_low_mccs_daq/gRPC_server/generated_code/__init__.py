1a57bbcd4dff7292fa813f38effdb1e0432dfb16
Adam Clemens <ajc@observatorysciences.co.uk>
2023-02-06 16:46:23 +0000

    [MCCS-1326] Test docfix 2

---
+++ src/ska_low_mccs_daq/gRPC_server/generated_code/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of the SKA Low MCCS project
+#
+#
+# Distributed under the terms of the BSD 3-clause new license.
+# See LICENSE for more info.
+"""This subpackage implements Daq gRPC Server functionality for MCCS."""
+
+
+__all__ = [
+    "daq_pb2",
+    "daq_pb2_grpc",
+]
+
+from . import daq_pb2, daq_pb2_grpc
