1a94a023da6a0cfb72191f3ce96ae31f2a67016c
Adam Clemens <ajc@observatorysciences.co.uk>
2023-02-06 12:31:43 +0000

    [MCCS-1326] Update docs.

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -57,6 +57,7 @@ autodoc_mock_imports = [
     "h5py",
     "matplotlib",
     "pydaq",
+    "grpc",
 ]
 
 
@@ -302,4 +303,5 @@ intersphinx_mapping = {
         "https://developer.skao.int/projects/ska-low-mccs-common/en/latest/",
         None,
     ),
+    "grpc": ("https://grpc.io/docs/"),
 }
