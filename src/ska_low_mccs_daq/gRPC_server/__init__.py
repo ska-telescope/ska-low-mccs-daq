bba39e5f3d35e5a8309e6fd7b369be58e9738665
Adam Clemens <ajc@observatorysciences.co.uk>
2023-02-07 15:16:42 +0000

    [MCCS-1326] Tweaking imports

--- src/ska_low_mccs_daq/gRPC_server/__init__.py
+++ src/ska_low_mccs_daq/gRPC_server/__init__.py
@@ -14,4 +14,5 @@ __all__ = [
     "daq_pb2_grpc",
 ]
 
-from .daq_grpc_server import MccsDaqServer, daq_pb2, daq_pb2_grpc
+from .daq_grpc_server import MccsDaqServer
+from .generated_code import daq_pb2, daq_pb2_grpc
