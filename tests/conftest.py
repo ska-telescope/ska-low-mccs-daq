19b42870ee8bc3ca3625506f6d0893bca61e1735
Adam Clemens <ajc@observatorysciences.co.uk>
2023-05-30 14:13:16 +0000

    [MCCS-1397] ...

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,132 +11,132 @@ This module contains pytest fixtures other test setups.
 These are common to all ska-low-mccs tests: unit, integration and
 functional (BDD).
 """
-from __future__ import annotations
+# from __future__ import annotations
 
-import logging
-import unittest
-from typing import Set, cast
+# import logging
+# import unittest
+# from typing import Set, cast
 
-import _pytest
-import pytest
-import tango
-import yaml
-
-
-def pytest_sessionstart(session: pytest.Session) -> None:
-    """
-    Pytest hook; prints info about tango version.
-
-    :param session: a pytest Session object
-    """
-    print(tango.utils.info())
+# import _pytest
+# import pytest
+# import tango
+# import yaml
 
 
-with open("tests/testbeds.yaml", "r", encoding="utf-8") as stream:
-    _testbeds: dict[str, set[str]] = yaml.safe_load(stream)
+# def pytest_sessionstart(session: pytest.Session) -> None:
+#     """
+#     Pytest hook; prints info about tango version.
+
+#     :param session: a pytest Session object
+#     """
+#     print(tango.utils.info())
 
 
-# TODO: https://github.com/pytest-dev/pytest-forked/issues/67
-# We're stuck on pytest 6.2 until this gets fixed,
-# and this version of pytest is not fully typehinted,
-# so we have to typehint against a private class for now.
-def pytest_configure(config: _pytest.config.Config) -> None:
-    """
-    Register custom markers to avoid pytest warnings.
-
-    :param config: the pytest config object
-    """
-    all_tags: Set[str] = cast(Set[str], set()).union(*_testbeds.values())
-    for tag in all_tags:
-        config.addinivalue_line("markers", f"needs_{tag}")
+# with open("tests/testbeds.yaml", "r", encoding="utf-8") as stream:
+#     _testbeds: dict[str, set[str]] = yaml.safe_load(stream)
 
 
-# TODO: https://github.com/pytest-dev/pytest-forked/issues/67
-# We're stuck on pytest 6.2 until this gets fixed,
-# and this version of pytest is not fully typehinted,
-# so we have to typehint against a private class for now.
-def pytest_addoption(parser: _pytest.config.argparsing.Parser) -> None:
-    """
-    Implement the add the `--testbed` option.
+# # TODO: https://github.com/pytest-dev/pytest-forked/issues/67
+# # We're stuck on pytest 6.2 until this gets fixed,
+# # and this version of pytest is not fully typehinted,
+# # so we have to typehint against a private class for now.
+# def pytest_configure(config: _pytest.config.Config) -> None:
+#     """
+#     Register custom markers to avoid pytest warnings.
 
-    Used to specify the context in which the test is running.
-    This could be used, for example, to skip tests that
-    have requirements not met by the context.
-
-    :param parser: the command line options parser
-    """
-    parser.addoption(
-        "--testbed",
-        choices=_testbeds.keys(),
-        default="test",
-        help="Specify the testbed on which the tests are running.",
-    )
+#     :param config: the pytest config object
+#     """
+#     all_tags: Set[str] = cast(Set[str], set()).union(*_testbeds.values())
+#     for tag in all_tags:
+#         config.addinivalue_line("markers", f"needs_{tag}")
 
 
-# TODO: https://github.com/pytest-dev/pytest-forked/issues/67
-# We're stuck on pytest 6.2 until this gets fixed,
-# and this version of pytest is not fully typehinted,
-# so we have to typehint against a private class for now.
-def pytest_collection_modifyitems(
-    config: _pytest.config.Config,
-    items: list[pytest.Item],
-) -> None:
-    """
-    Modify the list of tests to be run, after pytest has collected them.
+# # TODO: https://github.com/pytest-dev/pytest-forked/issues/67
+# # We're stuck on pytest 6.2 until this gets fixed,
+# # and this version of pytest is not fully typehinted,
+# # so we have to typehint against a private class for now.
+# def pytest_addoption(parser: _pytest.config.argparsing.Parser) -> None:
+#     """
+#     Implement the add the `--testbed` option.
 
-    This hook implementation skips tests that are marked as needing some
-    tag that is not provided by the current test context, as specified
-    by the "--testbed" option.
+#     Used to specify the context in which the test is running.
+#     This could be used, for example, to skip tests that
+#     have requirements not met by the context.
 
-    For example, if we have a hardware test that requires the presence
-    of a real TPM, we can tag it with "@needs_tpm". When we run in a
-    "test" context (that is, with "--testbed test" option), the test
-    will be skipped because the "test" context does not provide a TPM.
-    But when we run in "pss" context, the test will be run because the
-    "pss" context provides a TPM.
-
-    :param config: the pytest config object
-    :param items: list of tests collected by pytest
-    """
-    testbed = config.getoption("--testbed")
-    available_tags = _testbeds.get(testbed, set())
-
-    prefix = "needs_"
-    for item in items:
-        needs_tags = set(
-            tag[len(prefix) :] for tag in item.keywords if tag.startswith(prefix)
-        )
-        unmet_tags = list(needs_tags - available_tags)
-        if unmet_tags:
-            item.add_marker(
-                pytest.mark.skip(
-                    reason=(
-                        f"Testbed '{testbed}' does not meet test needs: "
-                        f"{unmet_tags}."
-                    )
-                )
-            )
+#     :param parser: the command line options parser
+#     """
+#     parser.addoption(
+#         "--testbed",
+#         choices=_testbeds.keys(),
+#         default="test",
+#         help="Specify the testbed on which the tests are running.",
+#     )
 
 
-@pytest.fixture(name="initial_mocks")
-def initial_mocks_fixture() -> dict[str, unittest.mock.Mock]:
-    """
-    Fixture that registers device proxy mocks prior to patching.
+# # TODO: https://github.com/pytest-dev/pytest-forked/issues/67
+# # We're stuck on pytest 6.2 until this gets fixed,
+# # and this version of pytest is not fully typehinted,
+# # so we have to typehint against a private class for now.
+# def pytest_collection_modifyitems(
+#     config: _pytest.config.Config,
+#     items: list[pytest.Item],
+# ) -> None:
+#     """
+#     Modify the list of tests to be run, after pytest has collected them.
 
-    By default no initial mocks are registered, but this fixture can be
-    overridden by test modules/classes that need to register initial
-    mocks.
+#     This hook implementation skips tests that are marked as needing some
+#     tag that is not provided by the current test context, as specified
+#     by the "--testbed" option.
 
-    :return: an empty dictionary
-    """
-    return {}
+#     For example, if we have a hardware test that requires the presence
+#     of a real TPM, we can tag it with "@needs_tpm". When we run in a
+#     "test" context (that is, with "--testbed test" option), the test
+#     will be skipped because the "test" context does not provide a TPM.
+#     But when we run in "pss" context, the test will be run because the
+#     "pss" context provides a TPM.
+
+#     :param config: the pytest config object
+#     :param items: list of tests collected by pytest
+#     """
+#     testbed = config.getoption("--testbed")
+#     available_tags = _testbeds.get(testbed, set())
+
+#     prefix = "needs_"
+#     for item in items:
+#         needs_tags = set(
+#             tag[len(prefix) :] for tag in item.keywords if tag.startswith(prefix)
+#         )
+#         unmet_tags = list(needs_tags - available_tags)
+#         if unmet_tags:
+#             item.add_marker(
+#                 pytest.mark.skip(
+#                     reason=(
+#                         f"Testbed '{testbed}' does not meet test needs: "
+#                         f"{unmet_tags}."
+#                     )
+#                 )
+#             )
 
 
-@pytest.fixture(scope="session", name="logger")
-def logger_fixture() -> logging.Logger:
-    """
-    Fixture that returns a default logger.
+# @pytest.fixture(name="initial_mocks")
+# def initial_mocks_fixture() -> dict[str, unittest.mock.Mock]:
+#     """
+#     Fixture that registers device proxy mocks prior to patching.
 
-    :return: a logger
-    """
-    return logging.getLogger()
+#     By default no initial mocks are registered, but this fixture can be
+#     overridden by test modules/classes that need to register initial
+#     mocks.
+
+#     :return: an empty dictionary
+#     """
+#     return {}
+
+
+# @pytest.fixture(scope="session", name="logger")
+# def logger_fixture() -> logging.Logger:
+#     """
+#     Fixture that returns a default logger.
+
+#     :return: a logger
+#     """
+#     return logging.getLogger()
