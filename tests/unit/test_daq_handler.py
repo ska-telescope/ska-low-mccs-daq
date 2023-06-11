4925992f0235957c44e8d997bbed333a2f398dea
Drew Devereux <drew.devereux@csiro.au>
2023-06-11 04:19:02 +0000

    [MCCS-1534] Refactor DAQ handler unit tests

--- tests/unit/test_daq_handler.py
+++ tests/unit/test_daq_handler.py
@@ -12,12 +12,10 @@ import gc
 import json
 from typing import Any
 
-import grpc
 import pytest
-from google.protobuf.json_format import MessageToDict
-from ska_control_model import ResultCode
+from ska_control_model import ResultCode, TaskStatus
 
-from ska_low_mccs_daq.interface.generated_code import daq_pb2, daq_pb2_grpc
+from ska_low_mccs_daq.interface.client import DaqClient
 
 # TODO: [MCCS-1211] Workaround for ska-tango-testing bug.
 gc.disable()
@@ -27,18 +25,22 @@ class TestDaqHandler:
     """Test class for DaqHandler tests."""
 
     @pytest.mark.parametrize(
-        ("args", "expected_rc", "expected_msg"),
+        ("args", "expected_status", "expected_msg"),
         (
-            ("", ResultCode.OK, "Daq started"),
-            ("invalidInput", ResultCode.FAILED, "Invalid DaqMode supplied"),
+            ("", TaskStatus.COMPLETED, "Daq has been started and is listening"),
+            pytest.param(
+                "invalidInput",
+                TaskStatus.FAILED,
+                "Invalid DaqMode supplied",
+                marks=pytest.mark.xfail(reason="Error propagation is not implemented."),
+            ),
         ),
     )
-    @pytest.mark.xfail
     def test_daq_server_start_stop_daq(
         self: TestDaqHandler,
         grpc_channel: str,
         args: str,
-        expected_rc: ResultCode,
+        expected_status: ResultCode,
         expected_msg: str,
     ) -> None:
         """
@@ -46,22 +48,34 @@ class TestDaqHandler:
 
         :param grpc_channel: The gRPC channel to communicate on.
         :param args: The argument with which to call `StartDaq`.
-        :param expected_rc: The result code expected from `StartDaq`.
+        :param expected_status: The expected task status expected from `StartDaq`.
         :param expected_msg: The message expected from `StartDaq`.
         """
-        with grpc.insecure_channel(grpc_channel) as channel:
-            stub = daq_pb2_grpc.DaqStub(channel)
-            response_init = stub.InitDaq(daq_pb2.configDaqRequest(config="{}"))
-            assert response_init.result_code == ResultCode.OK
-            assert response_init.message == "Daq successfully initialised"
+        daq_client = DaqClient(grpc_channel)
+        assert daq_client.initialise("{}") == {
+            "message": "Daq successfully initialised"
+        }
 
-            response_start = stub.StartDaq(daq_pb2.startDaqRequest(modes_to_start=args))
-            assert response_start.result_code == expected_rc
-            assert response_start.message == expected_msg
+        responses = daq_client.start_daq(args)
 
-            response_stop = stub.StopDaq(daq_pb2.stopDaqRequest())
-            assert response_stop.result_code == ResultCode.OK
-            assert response_stop.message == "Daq stopped"
+        assert next(responses) == {
+            "status": TaskStatus.IN_PROGRESS,
+            "message": "Start Command issued to gRPC stub",
+        }
+        assert next(responses) == {
+            "status": expected_status,
+            "message": expected_msg,
+        }
+
+        # If we were actually sending data for the DAQ to acquire,
+        # then here we could do something like
+        #
+        # assert next(responses) == {"types": "foo", "files": "foo.hdf5"}
+        # assert next(responses) == {"types": "bah", "files": "bah.hdf5"}
+        # with pytest.raises(StopIteration):
+        #     _ = next(responses)
+
+        assert daq_client.stop_daq() == (ResultCode.OK, "Daq stopped")
 
     @pytest.mark.parametrize(
         ("daq_config", "expected_rc", "expected_msg"),
@@ -95,34 +109,22 @@ class TestDaqHandler:
         :param expected_rc: The result code expected from `Configure`.
         :param expected_msg: The message expected from `Configure`.
         """
-        with grpc.insecure_channel(grpc_channel) as channel:
-            stub = daq_pb2_grpc.DaqStub(channel)
-            response_init = stub.InitDaq(daq_pb2.configDaqRequest(config="{}"))
-            assert response_init.result_code == ResultCode.OK
-            assert response_init.message == "Daq successfully initialised"
+        daq_client = DaqClient(grpc_channel)
+        assert daq_client.initialise("{}") == {
+            "message": "Daq successfully initialised"
+        }
 
-            # Assert initial config is not the final config if we're applying a new one.
-            response_initial_config = stub.GetConfiguration(daq_pb2.getConfigRequest())
-            response_initial_config_dict = MessageToDict(
-                message=response_initial_config, preserving_proto_field_name=True
-            )
+        initial_config = daq_client.get_configuration()
+        if daq_config != "":
+            for k, v in daq_config.items():
+                assert initial_config[k] != v
 
-            if daq_config != "":
-                for k, v in daq_config.items():
-                    assert response_initial_config_dict[k] != v
+        assert daq_client.configure_daq(json.dumps(daq_config)) == (
+            expected_rc,
+            expected_msg,
+        )
 
-            # Configure Daq (or try to)
-            response_config = stub.ConfigureDaq(
-                daq_pb2.configDaqRequest(config=json.dumps(daq_config))
-            )
-            assert response_config.result_code == expected_rc
-            assert response_config.message == expected_msg
-
-            # Check config was applied (if we applied one) by reading it back.
-            response_getconfig = stub.GetConfiguration(daq_pb2.getConfigRequest())
-            response_getconfig_dict = MessageToDict(
-                message=response_getconfig, preserving_proto_field_name=True
-            )
-            if daq_config != "":
-                for k, v in daq_config.items():
-                    assert response_getconfig_dict[k] == v
+        config = daq_client.get_configuration()
+        if daq_config != "":
+            for k, v in daq_config.items():
+                assert config[k] == v
