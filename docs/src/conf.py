82729c65d2df143f6fe720b22301b8f09aa16292
Adam Clemens <ajc@observatorysciences.co.uk>
2023-10-23 15:49:30 +0000

    [MCCS-1631] mock import "pyaavs"

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -28,6 +28,7 @@ sys.path.insert(0, os.path.abspath("../../src"))
 autodoc_mock_imports = [
     "h5py",
     "pydaq",
+    "pyaavs",
     "past",
     "matplotlib",
     "ska_low_mccs_daq_interface",
