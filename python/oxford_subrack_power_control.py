31a65f6a5c201133fc97e015fc1bf43b0cdb93f1
Peter Duffin <peter.duffin@stfc.ac.uk>
2024-03-01 14:57:26 +0000

    RAL subrack IP address changed

--- python/oxford_subrack_power_control.py
+++ python/oxford_subrack_power_control.py
@@ -3,7 +3,10 @@ from time import sleep
 
 # TODO: Add method to report which slots of ON or OFF      
  
-RAL_SUBRACK_1 = "10.0.10.32"
+RAL_SUBRACK_1 = "10.0.10.14"
+# RAL_SUBRACK_2 = "10.0.10.34"
+# RAL_SURBACK_3 = "10.0.10.54"
+# RAL_SUBRACK_4 = "10.0.10.74"
 OXFORD_SUBRACK_1 = "10.0.10.64"
 
 class Subrack:
