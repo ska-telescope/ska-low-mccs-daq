7125f2c425e8cef94d4dc249fcf7cc5db4d87d05
dmb74885 <john.holden@stfc.ac.uk>
2024-07-26 13:16:51 +0100

    REL-1634: Prepare for release v1.2.0

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aavs-system',
-    version='1.1',
+    version='1.2.0',
     packages=find_packages(),
     url='https://bitbucket.org/aavslmc/aavs-system',
     license='',
