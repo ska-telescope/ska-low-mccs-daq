6190908f2f101fbc26c7e18425f9b82fca483da3
Peter Duffin <peter.duffin@stfc.ac.uk>
2024-09-18 18:12:05 +0100

    SKB-526: Prepare for release 1.2.4

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aavs-system',
-    version='1.2.3',
+    version='1.2.4',
     packages=find_packages(),
     url='https://bitbucket.org/aavslmc/aavs-system',
     license='',
