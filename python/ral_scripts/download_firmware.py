29776985889b59e9420c9735b672120a0b54c467
dmb74885 <john.holden@stfc.ac.uk>
2024-11-13 08:41:04 +0000

    MCCS-2162: added comments to download_firmware.py

--- python/ral_scripts/download_firmware.py
+++ python/ral_scripts/download_firmware.py
@@ -25,6 +25,12 @@ class FirmwareManager:
         self.bitfile_pattern = bitfile_pattern
 
     def file_to_version(self, file_path):
+        """
+        Converts a file path to a version in the format x.y.z
+
+        This searches the file path for the number based on self.bitfile_pattern
+        It then removes any _, and the adds the .s
+        """
         try:
             search_pattern = self.bitfile_pattern.replace('.', '\.')
             search_pattern = search_pattern.replace('*', '(.*)')
@@ -37,6 +43,12 @@ class FirmwareManager:
             return None
 
     def version_to_file(self, version, extension=".bit"):
+        """
+        Converts a version number, to a file path based on
+        self.bitfiles_path, self.bitfile_pattern and the extension argument
+
+        The version nuber is converted from the format x.y.z to x_y_z
+        """
         bitfile_path = self.bitfiles_path / self.bitfile_pattern
         version_underscore = version.replace(".", "_")
         bitfile_path = str(bitfile_path).replace("*", version_underscore)
@@ -44,6 +56,16 @@ class FirmwareManager:
         return Path(file_path)
 
     def get_available_firmware_versions(self):
+        """
+        Gets a list of all firmware available in the car,
+        and updates the variables:
+
+        self.firmware_versions
+        self.latest_firmware_version
+        self.downloaded_firmware_versions
+        self.not_downloaded_firmware_versions
+        """
+
         shutil.rmtree(self.temp_path, ignore_errors=True)
         self.temp_path.mkdir()
         artifact_repo_url = "https://artefact.skao.int/service/rest/repository/browse/raw-internal/"
@@ -70,53 +92,70 @@ class FirmwareManager:
         return self.firmware_versions
 
     def get_downloaded_firmware_versions(self):
+        """
+        Search self.bitfiles_path folder for self.bitfile_pattern, then converts them into version numbers
+
+        Returns a list of version numbers
+        """
         file_list = list(self.bitfiles_path.glob(self.bitfile_pattern))
         return [self.file_to_version(file) for file in file_list if self.file_to_version(file) is not None]
 
     @staticmethod
     def split_version_num(version):
+        """
+        Splits a version number of x.y.z into a tuple
+        """
         return tuple(map(int, version.split('.')))
 
-    def is_minor_version(self, version):
-        return self.split_version_num(version)[2] == 0
-
-    def is_major_version(self, version):
-        return self.split_version_num(version)[2] == 0 and self.split_version_num(version)[1] == 0
-
     def download_firmware(self, include_ltx=False):
+        """
+        Gets the available firmware from the car, and downloads any that aren't already downloaded.
+        """
+
         self.get_available_firmware_versions()
         firmware_to_download_list = self.not_downloaded_firmware_versions
 
         for firmware_version in firmware_to_download_list:
-            firmware_version_path = Path(str(self.version_to_file(firmware_version)).replace(".bit", ""))
-            self.download_firmware_single(firmware_version, firmware_version_path, include_ltx)
+            firmware_bitstream_path = Path(str(self.version_to_file(firmware_version)))
+            self.download_firmware_single(firmware_version, firmware_bitstream_path, include_ltx)
 
         if not firmware_to_download_list:
             print("Firmware is all up to date")
         else:
             print(f"The following firmware has been downloaded: {', '.join(firmware_to_download_list)}")
 
-    def download_firmware_single(self, firmware_version, file_path, include_ltx=False):
+    def download_firmware_single(self, firmware_version, bitstream_path, include_ltx=False):
+        """
+        Downloads an individual version of the firmware from the car
+
+        First a temp directory is made using the path self.temp_path
+        The correct tar.gz file is then extracted and then the correct files are copied to their final place
+        Then the temp directory is removed
+        """
+
         shutil.rmtree(self.temp_path, ignore_errors=True)
         self.temp_path.mkdir()
         url = f"https://artefact.skao.int/repository/raw-internal/ska-low-sps-tpm-fpga-{firmware_version}.tar.gz"
         response = requests.get(url, timeout=15, stream=True)
         response.raise_for_status()  # Check if the download was successful
 
-        with open(os.path.join(self.temp_path, "files.tar.gz"), "wb") as f:
+        with open(os.path.join(self.temp_path, f"firmware_{firmware_version}.tar.gz"), "wb") as f:
             f.write(response.content)
 
-        with tarfile.open(os.path.join(self.temp_path, "files.tar.gz"), "r:gz") as tar:
+        with tarfile.open(os.path.join(self.temp_path, f"firmware_{firmware_version}.tar.gz"), "r:gz") as tar:
             tar.extractall(self.temp_path)
 
-        shutil.copy(self.temp_path/"tpm_firmware.bit", f"{file_path}.bit")
+        shutil.copy(self.temp_path/"tpm_firmware.bit", f"{bitstream_path}")
 
         if include_ltx:
-            shutil.copy(self.temp_path/"tpm_firmware.bit", f"{file_path}.ltx")
+            shutil.copy(self.temp_path/"tpm_firmware.bit", f"{str(bitstream_path).replace('.bit', '')}.ltx")
 
         shutil.rmtree(self.temp_path)
 
     def get_latest_firmware_version(self):
+        """
+        Returns the latest available firmware version on the car
+        """
         self.get_available_firmware_versions()
         return self.latest_firmware_version
 
