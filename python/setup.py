2bc0ea3fb8e3c804d0b80c3f0dd317bf82fb07b2
Peter Duffin <peter.duffin@stfc.ac.uk>
2024-11-26 13:23:31 +0000

    MCCS-2163: Prepare for release 2.0.1

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aavs-system',
-    version='2.0.0',
+    version='2.0.1',
     packages=find_packages(),
     url='https://gitlab.com/ska-telescope/aavs-system',
     license='',
