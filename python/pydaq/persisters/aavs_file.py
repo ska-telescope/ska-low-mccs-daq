79fb274a57c47a77c890d9f8d242dd4592c316ee
Tom Moynihan <tdm@observatorysciences.co.uk>
2025-01-16 15:18:15 +0000

    THORN-10: Pull from observation_data

--- python/pydaq/persisters/aavs_file.py
+++ python/pydaq/persisters/aavs_file.py
@@ -476,7 +476,7 @@ class AAVSFileManager(object):
                 metadata_dict["n_stokes"] = self.n_stokes
                 metadata_dict["channel_id"] = self.channel_id
                 metadata_dict["station_id"] = self.station_id
-                metadata_dict["description"] = self.description
+                metadata_dict["observation_data"] = file_obj["observation_data"]
                 if self.tsamp is not None:
                     metadata_dict["tsamp"] = self.tsamp
                 # metadata_dict["data_mode"] = str(self.data_mode)
@@ -836,9 +836,6 @@ class AAVSFileManager(object):
 
                         if 'station_id' in list(self.main_dset.attrs.keys()):
                             self.station_id = self.main_dset.attrs['station_id']
-
-                        if 'description' in list(self.main_dset.attrs.keys()):
-                            self.description = self.main_dset.attrs['description']
 
                         self.timestamp = self.main_dset.attrs['timestamp']
                         self.data_type_name = self.main_dset.attrs['data_type']
