2b9399ba63bd16833b61116778bc64db5f3c2599
Tom Moynihan <tdm@observatorysciences.co.uk>
2026-07-15 13:19:58 +0100

    THORN-601: Add GPU unit tests

--- src/ska_low_mccs_daq/cdaq/TensorCorrelatorData.cpp
+++ src/ska_low_mccs_daq/cdaq/TensorCorrelatorData.cpp
@@ -348,3 +348,6 @@ void TensorCrossCorrelator::threadEntry()
             read_samples, nof_packets, sample_bytes / (1024.0 * 1024.0));
     }
 }
+
+// Class factory for birales
+extern "C" DataConsumer *tensorcorrelator() { return new TensorCorrelatorData; }
